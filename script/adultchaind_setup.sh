9a4f981396fe9128a8632c73f2e9206e876c06eb
Tobias Beuving <informatiebureau@ehhh.nl>
2019-02-27 17:11:43 +0100

    install daemon

--- script/adultchaind_setup.sh
+++ script/adultchaind_setup.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
+
+# deps
+apt-get update
+apt-get upgrade -y
+apt-get install wget tar nano unrar unzip libboost-all-dev libevent-dev software-properties-common -y
+add-apt-repository ppa:bitcoin/bitcoin -y
+apt-get update
+apt-get install libdb4.8-dev libdb4.8++-dev -y
+
+# Swap
+fallocate -l 1500M /mnt/1500MB.swap
+dd if=/dev/zero of=/mnt/1500MB.swap bs=1024 count=1572864
+mkswap /mnt/1500MB.swap
+swapon /mnt/1500MB.swap
+chmod 600 /mnt/1500MB.swap
+echo '/mnt/1500MB.swap  none  swap  sw 0  0' >> /etc/fstab
+
+# Firewall
+ufw allow 22/tcp
+ufw limit 22/tcp
+ufw allow 6969/tcp
+ufw logging on
+ufw --force enable
+
 # Download latest node and install.
-#adultchainlink=`curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep browser_download_url | grep linux64 | cut -d '"' -f 4`
 mkdir -p /tmp/adultchain
 cd /tmp/adultchain
-#curl -Lo adultchain.tar.gz $adultchainlink
 wget https://github.com/zoldur/AdultChain/releases/download/v1.2.2.0/adultchain.tar.gz
 tar -xzf adultchain.tar.gz
 sudo mv adultchain-cli /usr/local/bin
