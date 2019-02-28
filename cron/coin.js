458866658af662a71adb31f9bfa3e792d1f805c8
Tobias Beuving <informatiebureau@ehhh.nl>
2019-02-28 22:35:26 +0100

    getting the global config stuff to work...

--- cron/coin.js
+++ cron/coin.js
@@ -1,5 +1,5 @@
 let currentPath = process.cwd();
-console.log('lookie:', currentPath);//__dirname );
+console.info('lookie:', currentPath);//__dirname );
 require('babel-polyfill');
 
 const { exit, rpc } = require('../lib/cron');
