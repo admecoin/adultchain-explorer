f75e501773225e4e4688b602ca71ef9e68f77e64
Tobias Beuving <informatiebureau@ehhh.nl>
2019-03-06 23:17:08 +0100

    Edited config.template.js

--- script/tmp/reset.sh
+++
@@ -1,5 +0,0 @@
-# When syncing was interrupted - clean up thses folders
-echo Cleaning up...
- rm -rf /root/.adultchain/blocks/
- rm -rf /root/.adultchain/chainstate
- rm -rf /root/.adultchain/peers.dat
