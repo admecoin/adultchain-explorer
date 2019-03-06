2a0dfc56fa293fcb9cb6bfbc11e0da313d3794bb
Tobias Beuving <informatiebureau@ehhh.nl>
2019-03-06 03:08:43 +0100

    Ditched the div/css based table and replaced it with a bootstrap html table

--- client/component/Card/CardEarnings.jsx
+++ client/component/Card/CardEarnings.jsx
@@ -1,4 +1,3 @@
-
 import blockchain from '../../../lib/blockchain';
 import numeral from 'numeral';
 import PropTypes from 'prop-types';
@@ -6,66 +5,109 @@ import React from 'react';
 import Card from './Card';
 import config from '../../../config.js';
 
-const CardEarnings = ({ coin }) => {
-  const subsidy = blockchain.getMNSubsidy(coin.blocks, coin.mnsOn, coin.supply);
-  const day = blockchain.getMNBlocksPerDay(coin.mnsOn) * subsidy;
-  const week = blockchain.getMNBlocksPerWeek(coin.mnsOn) * subsidy;
-  const month = blockchain.getMNBlocksPerMonth(coin.mnsOn) * subsidy;
-  const year = blockchain.getMNBlocksPerYear(coin.mnsOn) * subsidy;
+const CardEarnings = ({coin}) => {
+    const subsidy = blockchain.getMNSubsidy(coin.blocks, coin.mnsOn, coin.supply);
+    const day = blockchain.getMNBlocksPerDay(coin.mnsOn) * subsidy;
+    const week = blockchain.getMNBlocksPerWeek(coin.mnsOn) * subsidy;
+    const month = blockchain.getMNBlocksPerMonth(coin.mnsOn) * subsidy;
+    const year = blockchain.getMNBlocksPerYear(coin.mnsOn) * subsidy;
 
-  const ticker = `${config.project.ticker}`;
+    const ticker = `${config.project.ticker}`;
 
-  const ncoin = v => numeral(v).format('0,0');
-  const nbtc = v => numeral(v).format('0,0.00000000');
-  const nusd = v => numeral(v).format('$ 0,0.00');
+    const ncoin = v => numeral(v).format('0,0');
+    const nbtc = v => numeral(v).format('0,0.00000000');
+    const nusd = v => numeral(v).format('$ 0,0.00');
 
-  return (
-    <Card title="Estimated Earnings ( XXX / BTC / USD)">
-      <div className="row">
-        <div className="col-sm-12 col-md-3">
-          DAILY
-        </div>
-        <div className="col-sm-12 col-md-9">
-          { ncoin(day) } { ticker } / { nbtc(day * coin.btc) } BTC / { nusd(day * coin.usd) } USD
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-12 col-md-3">
-          WEEKLY
-        </div>
-        <div className="col-sm-12 col-md-9">
-          { ncoin(week) } { ticker } / { nbtc(week * coin.btc) } BTC / { nusd(week * coin.usd) } USD
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-12 col-md-3">
-          MONTHLY
-        </div>
-        <div className="col-sm-12 col-md-9">
-          { ncoin(month) } { ticker } / { nbtc(month * coin.btc) } BTC / { nusd(month * coin.usd) } USD
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-12 col-md-3">
-          YEARLY
-        </div>
-        <div className="col-sm-12 col-md-9">
-          { ncoin(year) } { ticker } / { nbtc(year * coin.btc) } BTC / { nusd(year * coin.usd) } USD
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <small className="u--text-gray">
-            * Estimates based on current block subsidy and active masternodes.
-          </small>
-        </div>
-      </div>
-    </Card>
-  );
+    return (
+        <Card title="Estimated Earnings">
+            <table className="table">
+                <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">{ticker}</th>
+                    <th scope="col">BTC</th>
+                    <th scope="col">USD</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">DAILY</th>
+                    <td>{ncoin(day)} </td>
+                    <td>{nbtc(day * coin.btc)}</td>
+                    <td>{nusd(day * coin.usd)}</td>
+                </tr>
+                <tr>
+                    <th scope="row">WEEKLY</th>
+                    <td>{ncoin(week)} </td>
+                    <td>{nbtc(week * coin.btc)}</td>
+                    <td>{nusd(week * coin.usd)}</td>
+                </tr>
+                <tr>
+                    <th scope="row">MONTHLY</th>
+                    <td>{ncoin(month)} </td>
+                    <td>{nbtc(month * coin.btc)}</td>
+                    <td>{nusd(month * coin.usd)}</td>
+                </tr>
+                <tr>
+                    <th scope="row">YEARLY</th>
+                    <td>{ncoin(year)} </td>
+                    <td>{nbtc(year * coin.btc)}</td>
+                    <td>{nusd(year * coin.usd)}</td>
+                </tr>
+                </tbody>
+            </table>
+        </Card>
+    );
 };
 
+/*
+{<Card title="Estimated Earnings ( XXX / BTC / USD)">
+    <div className="row">
+        <div className="col-sm-12 col-md-3">
+            DAILY
+        </div>
+        <div className="col-sm-12 col-md-9">
+            { ncoin(day) } { ticker } / { nbtc(day * coin.btc) } BTC / { nusd(day * coin.usd) } USD
+        </div>
+    </div>
+    <div className="row">
+        <div className="col-sm-12 col-md-3">
+            WEEKLY
+        </div>
+        <div className="col-sm-12 col-md-9">
+            { ncoin(week) } { ticker } / { nbtc(week * coin.btc) } BTC / { nusd(week * coin.usd) } USD
+        </div>
+    </div>
+    <div className="row">
+        <div className="col-sm-12 col-md-3">
+            MONTHLY
+        </div>
+        <div className="col-sm-12 col-md-9">
+            { ncoin(month) } { ticker } / { nbtc(month * coin.btc) } BTC / { nusd(month * coin.usd) } USD
+        </div>
+    </div>
+    <div className="row">
+        <div className="col-sm-12 col-md-3">
+            YEARLY
+        </div>
+        <div className="col-sm-12 col-md-9">
+            { ncoin(year) } { ticker } / { nbtc(year * coin.btc) } BTC / { nusd(year * coin.usd) } USD
+        </div>
+    </div>
+    <div className="row">
+        <div className="col">
+            <small className="u--text-gray">
+                * Estimates based on current block subsidy and active masternodes.
+            </small>
+        </div>
+    </div>
+</Card>
+}
+*/
+
+
 CardEarnings.propTypes = {
-  coin: PropTypes.object.isRequired
+    coin: PropTypes.object.isRequired
 };
 
 export default CardEarnings;
