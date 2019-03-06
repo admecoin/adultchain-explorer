131980000809e7c42b7b5429b0d9b31326dd482f
Tobias Beuving <informatiebureau@ehhh.nl>
2019-03-06 23:06:48 +0100

    Edited config.template.js

--- config.template.js
+++ config.template.js
@@ -1,22 +1,37 @@
 
 /**
- * Global configuration object.
+
+ * Global configuration object
+ * When building the client application with yarn,
+ * settings from a config.js file are used.
+ *
+ * !!!!!! The script/install_adultchain.sh creates and configures the config.js file automatically !!!!!!
+ *
+ *
+ * To manually configure (not recommended):
+ *
+ * Create a config.js file by copying this file and name the copy config.js
+ * From a commandline execute the following command:
+ * cp config.template.js config.js
+ *
+ * Open config.js and edit the values you need to change.
+ *
  */
 const config = {
   'project': {
-    'name': 'Bulwark',
-    'tagline': 'Masternodes & hardware',
-    'ticker': 'bwk'
+    'name': 'Name of your project',
+    'tagline': 'Unicorns For The Masses',
+    'ticker': 'UFTM'
   },
   'api': {
-    'host': 'https://explorer.bulwarkcrypto.com',
+    'host': 'https://explorer.unicorns-and-poop-rainbows.com',
     'port': '443',
     'prefix': '/api',
     'timeout': '5s'
   },
   'coinMarketCap': {
     'api': 'http://api.coinmarketcap.com/v1/ticker/',
-    'ticker': 'bulwark'
+    'ticker': 'unicornformasses'
   },
   'db': {
     'host': '127.0.0.1',
